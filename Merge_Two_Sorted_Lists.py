275c638f5bd5bad138c5ef9f546b73717d8a49c0
Kevin <knphillips01@gmail.com>
2020-12-21 20:41:54 -0600

    updated merge two sorted lists

--- Merge_Two_Sorted_Lists.py
+++ Merge_Two_Sorted_Lists.py
@@ -4,33 +4,37 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        # Start at the heads of l1 and l2 to see which value is less
-        # If one value is less, add to next nodes value
-        # Increment the pointer on that linked list and repeat the loop while l1 and l2 exists
-        # At end of loop, add whatever's left to output
-        # Base cases - if l1 doesn't exist, return l2 (no linked list on l1 -> return l2)
-        if not l1: return l2
-        # if no l2 => return l1
-        if not l2: return l1
-        current = dummy = ListNode(0) # dummy node
-        # create loop 
-        while l1 and l2:
-            #check to see if l1's value < l2
-            if l1.val < l2.val:
-                #Add to current next node
-                current.next = l1
-                #Increment l1 pointer   
-                l1 = l1.next
-                #otherwise, do the opposite
-            else:
-                current.next = l2
-                l2=l2.next
-                # make sure current pointer is incremented
-            current = current.next
-        # Add rest of l1 & l2 depending on what remains
-        if l1: current.next = l1
-        if l2: current.next = l2
+        # Create dummy node so don't have to worry about edge case of inserting into an empty list
+        dummy = ListNode(0)
+        # tail is normally the dummy
+        tail = dummy
         
+        # iterate through both lists with while loop
+        # If both of them are non-empty, that's when we can compare the two values
+        while l1 and l2:
+            # do comparison
+            if l1.val < l2.val:
+                #insert l1 value into tail
+                tail.next = l1
+                # update l1 pointer
+                l1=l1.next
+            else:
+                    # list 2 is less than or equal. Do exact same thing excecpt use list 2
+                tail.next = l2
+                    # update l2 pointer
+                l2 = l2.next
+                # update tail pointer - will be updated regardless of which node we insert into our list
+            tail = tail.next
+            
+            # edge case. What if one of lists is empty but the other is not?
+            # find the non-empty list and then insert into the end of our list
+            
+        if l1: #if l1 is not null
+            tail.next = l1
+        elif l2: # if l2 is not null
+            tail.next = l2
+            
         return dummy.next
